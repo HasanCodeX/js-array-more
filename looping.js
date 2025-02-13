d6f9f01a002b5aa4a052798c0fc1c9f7181bd32e
HasanCodeX <dev.hasan10@gmail.com>
2025-02-13 23:55:49 +0600

    first commit

---
+++ looping.js
@@ -0,0 +1,25 @@
+const friends = ['john', 'musk']
+for (const friend of friends ){
+    console.log(friend);
+}
+
+for (let i=0; i < 4; i++){
+    console.log(i)
+    console.log(friends[0])
+}
+
+
+const  numbers = [5,25,145,15,145,16,5,484,854,2]
+for (let i = 0; i < numbers.length; i++)
+    console.log (numbers[i])
+
+let i = 0;
+while (i < friends.length){
+    i++
+}
+
+let n = 0;
+while (n< numbers.length){
+    console.log ( numbers[n])
+    n++;
+}
